0446c94a48770d7173894a488ee5d01685d92ed9
victoriag2797 <victoriag2797@tuckahoeschools.org>
2024-12-03 13:26:31 +0000

    init commit

--- Loop Examples/forExample.js
+++ Loop Examples/forExample.js
@@ -1,1 +1,9 @@
+let outputDiv = document.getElementById("output");
+outputDiv.innerHTML += "<h2>Using a 'for' loop to create a star pattern</h2>";
 
+let pyramid = "";
+for(let i = 1; i < 300; i ++){
+    pyramid += "*".repeat(i) + "<br>";
+}
+
+outputDiv.innerHTML += pyramid;
