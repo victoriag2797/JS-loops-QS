3b4c45eb4aa771b7bdfddf805b6263a7f208d0c5
victoriag2797 <victoriag2797@tuckahoeschools.org>
2024-12-04 13:32:48 +0000

    commit

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,24 @@
+let outputDiv = document.getElementById("output");
+let passwordInput = document.getElementById("passwordInput");
+let submitButton = document.getElementById("submitButton");
 
+const correctPassword = "showtime";
+outputDiv.innerHTML += "<h2>Using a password prompt</h2>";
+
+submitButton.addEventListener("click", => ()=>{
+    let password;
+    let attempt = 0;
+
+    do{
+        password = passwordInput.value;
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "Acess Granted<br>"
+            passwordInput.disabled = true;
+            submitButton.disabled = true;
+            break;
+        }else{
+            outputDiv.innerHTML += "Incorrect Password. Attempt ${++attempt}. Try again <br>";
+        }
+    }
+});
---
+++ Loop Examples/whileExample.js
@@ -0,0 +1,10 @@
+let diceRoll;
+let rolls = 0;
+
+while(diceRoll !== 6){
+    diceRoll = Math.floor(Math.random() * 6) + 1;
+    rolls++;
+    console.log(`You rolled: ${diceRoll}`);
+}
+
+console.log(`It took ${rolls} rolls to get a 6!`);
